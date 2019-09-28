35a39c85cd92aa1c50e319cb4b3e46648f569405
unknown <gyhgithub@163.com>
2019-09-28 18:40:16 +0800

    Initial commit

---
+++ src/main/java/com/example/employeelogs/EmployeelogsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeelogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeelogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeelogsApplication.class, args);
+    }
+
+}
